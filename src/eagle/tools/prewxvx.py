d358cc3194c00d0f2a2efd5dc5a5bb6304541001
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-11-28 21:50:37 -0800

    move nested specific stuff to its own module

--- src/eagle/tools/prewxvx.py
+++ src/eagle/tools/prewxvx.py
@@ -14,84 +14,6 @@ from eagle.tools.data import open_anemoi_inference_dataset, open_forecast_zarr_d
 
 logger = logging.getLogger("eagle.tools")
 
-def _rename(xds: xr.Dataset) -> xr.Dataset:
-    rename = {"latitude": "lat", "longitude": "lon"}
-    for key, val in rename.items():
-        if key in xds:
-            xds = xds.rename({key: val})
-    return xds
-
-
-def prepare_regrid_target_mask(
-    anemoi_reference_dataset_kwargs: dict,
-    horizontal_regrid_kwargs: dict,
-) -> str:
-    """check if the target mask is there, otherwise add it and store it in a new spot"""
-
-    target_grid_path = os.path.expandvars(horizontal_regrid_kwargs["target_grid_path"])
-    regridder_kwargs = horizontal_regrid_kwargs["regridder_kwargs"]
-
-    kw = horizontal_regrid_kwargs.get("open_target_kwargs", {})
-    tds = xr.open_dataset(target_grid_path, **kw)
-    tds = _rename(tds)
-    if "mask" not in tds.data_vars and "mask" not in tds.coords:
-        logger.info(f"Could not find mask in target dataset, but it's needed for conservative_normed regridding. Computing it now")
-
-        # Open anemoi dataset, get the mask and reshape it
-        ads = anemoi.datasets.open_dataset(**anemoi_reference_dataset_kwargs)
-        mask2d = ads.global_mask.reshape( (len(tds["lat"]), len(tds["lon"])) )
-        tds["mask"] = xr.DataArray(
-            np.where(mask2d, 0, 1),
-            dims=("lat", "lon"),
-        )
-
-        # store it
-        fileparts = os.path.splitext(target_grid_path)
-        target_grid_path = f"{fileparts[0]}_with_mask{fileparts[1]}"
-        tds.to_netcdf(target_grid_path)
-        logger.info(f"Stored {target_grid_path}")
-
-    return target_grid_path
-
-
-def regrid_nested_to_latlon(
-    xds: xr.Dataset,
-    lam_index: int,
-    lcc_info: dict,
-    horizontal_regrid_kwargs: dict,
-) -> xr.Dataset:
-
-    assert "regridder_kwargs" in horizontal_regrid_kwargs
-    assert "method" in horizontal_regrid_kwargs["regridder_kwargs"]
-    assert horizontal_regrid_kwargs["regridder_kwargs"]["method"] == "conservative_normed"
-
-    # Get the LAM portion and global without lam portion
-    lds = xds.isel(cell=slice(lam_index))
-    lds = reshape_cell_to_xy(lds, **lcc_info)
-    cds = xds.isel(cell=slice(lam_index, None))
-
-    # Regrid to global resolution, stack lat/lon, drop non LAM part
-    lam_on_global = horizontal_regrid(lds, **horizontal_regrid_kwargs)
-    lam_on_global = lam_on_global.stack(cell2d=("latitude", "longitude"))
-    lam_on_global = lam_on_global.swap_dims({"cell2d": "cell"})
-    lam_on_global = lam_on_global.drop_vars("cell2d")
-    lam_on_global = lam_on_global.dropna("cell")
-
-    # create a "cell" index for the two components
-    # note that these are dimensions without variable values at this point
-    cutout_cell = np.arange(len(cds.cell))
-    lam_cell = len(cutout_cell) + np.arange(len(lam_on_global.cell))
-    cds["cell"] = xr.DataArray(cutout_cell, coords={"cell": cutout_cell})
-    lam_on_global["cell"] = xr.DataArray(lam_cell, coords={"cell": lam_cell})
-
-    # concat global+lam, resort, and expand to latlon again
-    result = xr.concat([cds, lam_on_global], dim="cell")
-    sort_index = np.lexsort( (result["longitude"], result["latitude"]) )
-    result = result.isel(cell=sort_index)
-    result = reshape_cell_to_latlon(result)
-    return result
-
-
 def main(config):
 
     setup_simple_log()
@@ -100,7 +22,6 @@ def main(config):
     output_path = config["output_path"]
     model_type = config["model_type"]
     from_anemoi = config.get("from_anemoi", True)
-    anemoi_reference_dataset_kwargs = config.get("anemoi_reference_dataset_kwargs", None)
 
     open_kwargs = {
         "load": True,
@@ -111,12 +32,6 @@ def main(config):
     }
 
     if model_type == "nested-global":
-        assert anemoi_reference_dataset_kwargs is not None, \
-            f"Need to provide reference anemoi dataset to handle nested-global workflow"
-        assert "lam_index" in config
-        assert "lcc_info" in config
-        assert "horizontal_regrid_kwargs" in config
-
         config["horizontal_regrid_kwargs"]["target_grid_path"] = prepare_regrid_target_mask(
             anemoi_reference_dataset_kwargs=config["anemoi_reference_dataset_kwargs"],
             horizontal_regrid_kwargs=config["horizontal_regrid_kwargs"],
